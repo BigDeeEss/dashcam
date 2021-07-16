d86f3bc9c4d118a1f84c8e65632f49bae2b9129b
Damon Swatton <damon.swatton@gmail.com>
2021-07-16 21:46:40 +0100

    Define top level DashCam widget which is the entry point to the app.
    Add home.dart.
    
    Define an initial route pointing to Home() in home.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,49 @@
+
+
+// Import flutter packages.
 import 'package:flutter/material.dart';
 
+
+// Import project-specific files.
+import './home.dart';
+
+
+// App start point.
 void main() {
-  runApp(const BottomAppBarDemo());
+  runApp(const DashCam());
 }
 
-class BottomAppBarDemo extends StatefulWidget {
-  const BottomAppBarDemo({Key? key}) : super(key: key);
-  @override
-  State createState() => _BottomAppBarDemoState();
-}
 
-class _BottomAppBarDemoState extends State<BottomAppBarDemo> {
+class DashCam extends StatelessWidget {
+  // Default constructor required for const specifier
+  // in 'runApp(const DashCam());' above.
+  const DashCam({Key? key}) : super(key: key);
+
+  // Root widget of DashCam project.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: const Text('Bottom App Bar Demo'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-          tooltip: 'Create',
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.blue,
-          shape: CircularNotchedRectangle(),
-          child: IconTheme(
-            data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-            child: Row(
-              children: <Widget>[
-                IconButton(
-                  tooltip: 'Open navigation menu',
-                  icon: const Icon(Icons.menu),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      title: 'DashCam',
+
+      // Define an initialRoute that is executed on initial widget build.
+      initialRoute: '/',
+
+      // Define all routes in DashCam app; note this is a map/dictionary.
+      routes: {
+        // Specify initial route (built at time of initial widget build).
+        // Can be overridden by initialRoute.
+        '/': (context) => Home(),
+
+        // // Specify route => Home() route...
+        // '/home': (context) => Home(),
+        //
+        // // Specify route => Files() route...
+        // '/files': (context) => Files(),
+        //
+        // // Specify route => Settings() route...
+        // '/settings': (context) => Settings(),
+      },
     );
   }
 }
+
