36b4964c8b72dc0ea8d9c260e16a30c15156cc2e
Damon Swatton on PAVILION <damon.swatton@gmail.com>
2021-08-14 21:13:59 +0100

    Add SlideTransition to page transition folloeing FAB acivation.
    Problem: The base page appears to slide in and out but the buttons don't move.

--- lib/button_specs.dart
+++ lib/button_specs.dart
@@ -13,7 +13,16 @@ Route _createRoute(ButtonSpec buttonSpec) {
           title: buttonSpec.title,
         ),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return child;
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
     },
   );
 }
