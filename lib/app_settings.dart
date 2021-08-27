12720ca9452ce6e0c2fd240b04edcb699dc2eec2
Damon Swatton on ENVY <damon.swatton@gmail.com>
2021-08-27 19:34:51 +0100

    Add app_settings.dart.
    Add bottomNavigationBar to BasePage.
    
    The height of the BottomAppBar is determined by applying a scale factor to the height of the appBar. Since the height of the appBar is required, and whilst the BasePage is being built the scaffold height is not known, a Builder widget is required in order to extract the appBar height from its parent.

---
+++ lib/app_settings.dart
@@ -0,0 +1,6 @@
+//  Import flutter packages.
+import 'package:flutter/painting.dart';
+
+/// [appBarHeightScaleFactor] defines a simple scale factor to apply to
+/// appBar when calculating bottomNavigationBar in BasePage.
+const double appBarHeightScaleFactor = 1.0;
--- lib/base_page.dart
+++ lib/base_page.dart
@@ -1,14 +1,18 @@
 //  Import flutter packages.
 import 'package:flutter/material.dart';
 
-//  StatelessWidget which implements a basic page layout design.
+// Import project-specific files.
+import 'package:dashcam/app_settings.dart' as appSettings;
+
+/// [BasePage] implements a basic page layout design.
 class BasePage extends StatelessWidget {
   const BasePage({
     required this.title,
     Key? key,
   }) : super(key: key);
 
-  /// [title] is the page title displayed on the appBar.
+  /// [title] is the page title displayed on the appBar of the
+  /// [BasePage] widget.
   final String title;
 
   @override
@@ -16,6 +20,32 @@ class BasePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(this.title)
+      ),
+
+      //  Use Builder(...) widget because it is not possible to get the appBar
+      //  height from the current BuildContext when it doesn't yet include the
+      //  Scaffold(...) class being returned by this widget.
+      bottomNavigationBar: Builder(
+        builder: (BuildContext context) {
+          //  [height] variable for storing appBar height.
+          double height = MediaQuery.of(context).padding.top + kToolbarHeight;
+
+          return BottomAppBar(
+            color: Colors.blue,
+            child: Row(
+              children: <Widget>[
+                SizedBox(
+                  //  Set height of the BottomAppBar(...) class variable using
+                  //  SizedBox(...). Get height from [context] by first
+                  //  extracting the immediate Scaffold(...), and then getting
+                  //  the value for appBarMaxHeight. Need to take into account
+                  //  appBarMaxHeight possibly being null.
+                  height: height * appSettings.appBarHeightScaleFactor,
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
